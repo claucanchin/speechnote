4006145e40a7e3fa007121d4ed6e2a480d188aad
claudia <claucanchin@gmail.com>
2019-05-30 22:35:18 +0800

    ran npm run build in client folder

---
+++ client/build/precache-manifest.1a0569a34c9a32482b2c4b484c95a601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88ca38903497a2ca49da",
+    "url": "/static/css/main.9f6fa5be.chunk.css"
+  },
+  {
+    "revision": "88ca38903497a2ca49da",
+    "url": "/static/js/main.6511ad18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e1d5036797a259812ce8",
+    "url": "/static/js/2.df838ba6.chunk.js"
+  },
+  {
+    "revision": "72e30d3ff495b079fff3db7a775bb763",
+    "url": "/index.html"
+  }
+];
